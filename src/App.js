5d555fa9515311498a242ff3e52818c6742d044b
rondolphstm <rondolphtst.m@gmail.com>
2022-09-01 16:53:17 -0400

    secure task routes with jwt

--- src/App.js
+++ src/App.js
@@ -1,14 +1,57 @@
 import './App.css';
+import {Layout, Menu} from 'antd'
 import TodoList from './components/TodoList';
-import {useState} from 'react'
+import AddTask from './components/AddTask';
+import SignUp from './components/Signup'
+import {useState, useEffect} from 'react'
+
+const {Header, Content, Footer} = Layout
 
 function App() {
-  const [tasklist, setTasklist] = useState()
+  const [taskList, setTaskList] = useState()
+  const [token, setToken] = useState()
+  const[isUser , setIsUser] = useState()
+  useEffect(()=>{
+    if(localStorage.getItem("token")){
+      setToken(localStorage.getItem("token"))
+    }
+  },[setToken])
   return (
-    <>
-    <h1> Three-do </h1>
-    <TodoList tasklist={tasklist} setTasklist={setTasklist}/>
-    </>
+    <Layout className="layout">
+      <Header>
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={['1']}>
+          <Menu.Item key="1">Three-Do</Menu.Item>
+          <Menu.Item key="2">Login</Menu.Item>
+          <Menu.Item key="3">Sign Up</Menu.Item>
+        </Menu>
+      </Header>
+      <Content
+        style={{
+          padding: '0 50px',
+        }}
+      >
+        <div className="site-layout-content">
+          <h1>Three-do</h1>
+          <TodoList token={token} taskList={taskList} setTaskList={setTaskList} />
+          <AddTask token={token} setTaskList={setTaskList} />
+          {!token && 
+          isUser
+          <setToken={setToken}/>
+          <SignUp setToken={setToken}/>
+          }
+        </div>
+      </Content>
+      <Footer
+        style={{
+          textAlign: 'center',
+        }}
+      >
+        Three-Do ©2022 Created by Rondolph st.martin
+      </Footer>
+    </Layout>
   );
 }
 
